7e5b35e340145d165e6107bddb7c91a2b0a865a7
Ronaldo Veloso Filho <ronaldovf24@gmail.com>
2026-05-25 19:45:12 -0300

    SpringBoot setup

---
+++ src/main/java/br/com/fiap/space_mission/SpaceMissionApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.space_mission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceMissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceMissionApplication.class, args);
+	}
+
+}
